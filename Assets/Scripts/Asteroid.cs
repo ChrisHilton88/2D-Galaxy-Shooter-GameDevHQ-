692051c1079f5d4b412da2071c0e90ec9b0a4b85
Chris Hilton <christopherhilton88@gmail.com>
2021-07-08 16:01:13 +0200

    Completed the damage VFX for the engine thrusters and setting up the Post Processing for our game

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] private int rotateSpeed = 10;
+
+    Animator anim;
+
+    SpawnManager spawnManager;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spawnManager = GameObject.Find("Spawn Manager").GetComponent<SpawnManager>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 1 * rotateSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            anim.SetTrigger("OnAsteroidExplosion");
+            spawnManager.StartSpawning();
+            Destroy(this.gameObject, 2.633f);
+        }
+    }
+}
